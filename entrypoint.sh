7bf74979c1a5567884ccb4dd269fbc61e69572e9
Sid Kattoju <skattoju@redhat.com>
2022-11-09 13:02:10 -0500

    fix arg list

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 echo "bundle path: $1"
 echo "generate bundle: $2"
-bundle-validate-output=$(/usr/local/bin/operator-sdk/operator-sdk bundle validate $1)
+bundle-validate-output=$(/usr/local/bin/operator-sdk bundle validate $1)
 echo "bundle-validate-output=$validation-output" >> $GITHUB_OUTPUT
